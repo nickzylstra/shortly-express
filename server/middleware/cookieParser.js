37dc2d20aee4b3f9cdd905b72e5058b1a7ea2bcc
nickzylstra <nick.zylstra@gmail.com>
2019-11-15 16:29:55 -0800

    Creates first pass on cookieparser

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,12 @@
 const parseCookies = (req, res, next) => {
+  const { cookie } = req.headers;
+  const cookieDetais = cookie.split('; ');
+  const cookies = {};
+  cookieDetais.map((c) => c.split('=')).forEach(([name, hash]) => {
+    cookies[name] = hash;
+  });
+  req.cookies = cookies;
+  next();
 };
 
 module.exports = parseCookies;
