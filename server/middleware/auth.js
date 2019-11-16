1776553bbbcfb60de75ee93c195fe0cdbf97a348
nickzylstra <nick.zylstra@gmail.com>
2019-11-15 18:21:00 -0800

    Updates cookieparser to handle no cookies

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,21 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  const parsedCookies = req.cookies;
+  const ourCookieName = 'shortlyid';
+  const reqCookieVal = parsedCookies[ourCookieName];
+
+  // if (!reqCookieVal) {
+  //   models.Sessions.create()
+  // } else {
+  //   models.Sessions.get({hash: reqCookieVal})
+  //     .then((session) => {
+
+  //     })
+  //     .catch((err) => {
+
+  //     });
+  // }
 };
 
 /************************************************************/
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,10 +1,12 @@
 const parseCookies = (req, res, next) => {
   const { cookie } = req.headers;
-  const cookieDetais = cookie.split('; ');
   const cookies = {};
-  cookieDetais.map((c) => c.split('=')).forEach(([name, hash]) => {
-    cookies[name] = hash;
-  });
+  if (cookie) {
+    const cookieDetais = cookie.split('; ');
+    cookieDetais.map((c) => c.split('=')).forEach(([name, hash]) => {
+      cookies[name] = hash;
+    });
+  }
   req.cookies = cookies;
   next();
 };
